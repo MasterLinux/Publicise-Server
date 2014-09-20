006ad5c22fc4a678361c3aeef46ceca6388cd1b8
Christoph Grundmann <christoph.grundmann@inserteffect.com>
2014-09-20 15:52:25 +0200

    URI methods moved to an util class

--- server/src/main/java/net/apetheory/publicise/server/data/ResourceSet.java
+++ server/src/main/java/net/apetheory/publicise/server/data/ResourceSet.java
@@ -51,8 +51,7 @@ public class ResourceSet<TResource extends BaseResource> {
      * Builder used to build a ResourceSet
      */
     static public class Builder<TResource extends BaseResource> {
-        private static final int EMPTY_OFFSET_PARAMETER_VALUE = -1;
-        private static final int EMPTY_LIMIT_PARAMETER_VALUE = -1;
+
         private List<TResource> objects;
         private UriInfo uriInfo;
         private MetaModel meta;
@@ -148,7 +147,7 @@ public class ResourceSet<TResource extends BaseResource> {
          */
         private void setPrev(MetaModel meta, UriInfo uriInfo) {
             if (meta.getOffset() > 0) {
-                meta.setPrev(buildUriPath(UriType.Previous, uriInfo));
+                meta.setPrev(UriUtils.buildUriPath(UriType.Previous, uriInfo));
             }
         }
 
@@ -159,81 +158,8 @@ public class ResourceSet<TResource extends BaseResource> {
         private void setNext(MetaModel meta, UriInfo uriInfo) {
             long rest = meta.getTotalCount() - meta.getLimit() * (meta.getOffset() + 1);
             if(rest > 0) {
-                meta.setNext(buildUriPath(UriType.Next, uriInfo));
+                meta.setNext(UriUtils.buildUriPath(UriType.Next, uriInfo));
             }
-        }
-
-        private int getCurrentOffset(UriInfo uriInfo) {
-            MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
-
-            int offset = EMPTY_OFFSET_PARAMETER_VALUE;
-            if(queryParameters.containsKey("offset")) {
-                offset = Integer.parseInt(queryParameters.get("offset").get(0));
-            }
-
-            return offset;
-        }
-
-        private int getCurrentLimit(UriInfo uriInfo) {
-            MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
-
-            int limit = EMPTY_LIMIT_PARAMETER_VALUE;
-            if(queryParameters.containsKey("limit")) {
-                limit = Integer.parseInt(queryParameters.get("limit").get(0));
-            }
-
-            return limit;
-        }
-
-        private String buildUriPath(UriType type, UriInfo uriInfo) {
-            MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
-            int offset = getCurrentOffset(uriInfo);
-            int limit = getCurrentLimit(uriInfo);
-
-            UriBuilder builder =
-                    uriInfo.getAbsolutePathBuilder()
-                        .scheme(null)
-                        .host(null);
-
-            //reset all query parameters
-            for(Map.Entry<String, List<String>> entry : queryParameters.entrySet()) {
-                List<String> value = entry.getValue();
-                String name = entry.getKey();
-
-                if(isQueryParameterValid(value, name)) {
-                    builder.replaceQueryParam(entry.getKey(), value.get(0));
-                }
-            }
-
-            //set limit parameter
-            if(limit != EMPTY_LIMIT_PARAMETER_VALUE) {
-                builder.replaceQueryParam("limit", String.valueOf(limit));
-            }
-
-            //set offset parameter
-            if(offset != EMPTY_OFFSET_PARAMETER_VALUE) {
-                offset = type.equals(UriType.Previous) ? offset - 1 : offset + 1;
-                builder.replaceQueryParam("offset", String.valueOf(offset));
-            }
-
-            return builder.build().toString();
-        }
-
-        /**
-         * Checks whether the query parameter is valid
-         * @param value The value of the query parameter
-         * @param name The name of the query parameter
-         * @return true if it is valid, false otherwise
-         */
-        private boolean isQueryParameterValid(List<String> value, String name) {
-            return !name.equals("limit") &&
-                    !name.equals("offset") &&
-                    value.size() > 0 &&
-                    !StringUtils.isEmpty(value.get(0));
-        }
-
-        private enum UriType {
-            Previous, Next
         }
     }
 }
---
+++ server/src/main/java/net/apetheory/publicise/server/data/UriUtils.java
@@ -0,0 +1,118 @@
+package net.apetheory.publicise.server.data;
+
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Utility class which contains some helper methods
+ * for building and validating URIs
+ *
+ * @author Christoph Grundmann
+ */
+public class UriUtils {
+    public static final int EMPTY_OFFSET_PARAMETER_VALUE = -1;
+    public static final int EMPTY_LIMIT_PARAMETER_VALUE = -1;
+
+    /**
+     * Builds the URI path for a specific URI type
+     *
+     * @param type The type of the URI to build
+     * @param uriInfo The URI info object containing all URI info
+     * @return The relative URI
+     */
+    public static String buildUriPath(UriType type, UriInfo uriInfo) {
+        MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+        int offset = getCurrentOffset(uriInfo);
+        int limit = getCurrentLimit(uriInfo);
+
+        UriBuilder builder =
+                uriInfo.getAbsolutePathBuilder()
+                        .scheme(null)
+                        .host(null)
+                        .port(-1);
+
+        //reset all query parameters
+        for(Map.Entry<String, List<String>> entry : queryParameters.entrySet()) {
+            List<String> value = entry.getValue();
+            String name = entry.getKey();
+
+            if(isQueryParameterValid(name, value, "offset", "limit")) {
+                builder.replaceQueryParam(entry.getKey(), value.get(0));
+            }
+        }
+
+        //set limit parameter
+        if(limit != EMPTY_LIMIT_PARAMETER_VALUE) {
+            builder.replaceQueryParam("limit", String.valueOf(limit));
+        }
+
+        //set offset parameter
+        if(offset != EMPTY_OFFSET_PARAMETER_VALUE) {
+            if(!UriType.Self.equals(type)) {
+                offset = UriType.Previous.equals(type) ? offset - 1 : offset + 1;
+            }
+            builder.replaceQueryParam("offset", String.valueOf(offset));
+        }
+
+        return builder.build().toString();
+    }
+
+    /**
+     * Gets the current offset parameter value
+     *
+     * @param uriInfo The URI info object containing all URI info
+     * @return The current offset or UriUtils.EMPTY_OFFSET_PARAMETER_VALUE if field does not exist
+     */
+    public static int getCurrentOffset(UriInfo uriInfo) {
+        MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+
+        int offset = EMPTY_OFFSET_PARAMETER_VALUE;
+        if(queryParameters.containsKey("offset")) {
+            offset = Integer.parseInt(queryParameters.get("offset").get(0));
+        }
+
+        return offset;
+    }
+
+    /**
+     * Gets the current limit parameter value
+     *
+     * @param uriInfo The URI info object containing all URI info
+     * @return The current limit or UriUtils.EMPTY_LIMIT_PARAMETER_VALUE if field does not exist
+     */
+    public static int getCurrentLimit(UriInfo uriInfo) {
+        MultivaluedMap<String, String> queryParameters = uriInfo.getQueryParameters();
+
+        int limit = EMPTY_LIMIT_PARAMETER_VALUE;
+        if(queryParameters.containsKey("limit")) {
+            limit = Integer.parseInt(queryParameters.get("limit").get(0));
+        }
+
+        return limit;
+    }
+
+    /**
+     * Checks whether the given query parameter is valid
+     *
+     * @param value The value of the query parameter to test
+     * @param name The name of the query parameter to test
+     * @param excludes List of excluded names
+     * @return true if it is valid, false otherwise
+     */
+    public static boolean isQueryParameterValid(String name, List<String> value, String... excludes) {
+        boolean isExcluded = false;
+
+        for(String exclude : excludes) {
+            if(name.equals(exclude)) {
+                isExcluded = true;
+                break;
+            }
+        }
+
+        return !isExcluded && value.size() > 0 &&
+                !StringUtils.isEmpty(value.get(0));
+    }
+}
