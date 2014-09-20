6249bceadf979a02609d9e665660cf2e3c4e0874
Christoph Grundmann <christoph.grundmann@inserteffect.com>
2014-09-20 16:53:41 +0200

    offset and limit getter tests added

--- server/src/test/java/UriUtilsTest.java
+++ server/src/test/java/UriUtilsTest.java
@@ -2,10 +2,17 @@ import net.apetheory.publicise.server.data.UriUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.UriInfo;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.Mockito.*;
+
 public class UriUtilsTest {
+
+    private static final String EXPECTED_OFFSET_VALUE = "7";
+    private static final String EXPECTED_LIMIT_VALUE = "3";
 
     @Test
     public void testUriParameterShouldBeValid() {
@@ -52,5 +59,63 @@ public class UriUtilsTest {
 
         Assert.assertFalse(UriUtils.isQueryParameterValid(name, values));
         Assert.assertFalse(UriUtils.isQueryParameterValid(null, values, exclude));
+    }
+
+    @Test
+    public void testOffsetValueShouldBeAsExpected() {
+        MultivaluedHashMap<String, String> parameter = new MultivaluedHashMap<>();
+        parameter.add("offset", EXPECTED_OFFSET_VALUE);
+        UriInfo info = buildUriInfoMock(parameter);
+
+        Assert.assertEquals((Object) Integer.valueOf(EXPECTED_OFFSET_VALUE), UriUtils.getCurrentOffset(info));
+    }
+
+    @Test
+    public void testOffsetValueShouldBeUndefined() {
+        MultivaluedHashMap<String, String> parameter = new MultivaluedHashMap<>();
+        UriInfo info = buildUriInfoMock(parameter);
+        String emptyValue = "";
+
+        //test missing value
+        Assert.assertEquals((Object) UriUtils.UNDEFINED_OFFSET_PARAMETER_VALUE, UriUtils.getCurrentOffset(info));
+
+        parameter.add("offset", emptyValue);
+        info = buildUriInfoMock(parameter);
+
+        //test empty value
+        Assert.assertEquals((Object) UriUtils.UNDEFINED_OFFSET_PARAMETER_VALUE, UriUtils.getCurrentOffset(info));
+    }
+
+    @Test
+    public void testLimitValueShouldBeAsExpected() {
+        MultivaluedHashMap<String, String> parameter = new MultivaluedHashMap<>();
+        parameter.add("limit", EXPECTED_LIMIT_VALUE);
+        UriInfo info = buildUriInfoMock(parameter);
+
+        Assert.assertEquals((Object) Integer.valueOf(EXPECTED_LIMIT_VALUE), UriUtils.getCurrentLimit(info));
+    }
+
+    @Test
+    public void testLimitValueShouldBeUndefined() {
+        MultivaluedHashMap<String, String> parameter = new MultivaluedHashMap<>();
+        UriInfo info = buildUriInfoMock(parameter);
+        String emptyValue = "";
+
+        //test missing value
+        Assert.assertEquals((Object) UriUtils.UNDEFINED_LIMIT_PARAMETER_VALUE, UriUtils.getCurrentLimit(info));
+
+        parameter.add("limit", emptyValue);
+        info = buildUriInfoMock(parameter);
+
+        //test empty value
+        Assert.assertEquals((Object) UriUtils.UNDEFINED_LIMIT_PARAMETER_VALUE, UriUtils.getCurrentLimit(info));
+    }
+
+    private UriInfo buildUriInfoMock(MultivaluedHashMap<String, String> parameter) {
+        UriInfo uriInfo = mock(UriInfo.class);
+
+        when(uriInfo.getQueryParameters()).thenReturn(parameter);
+
+        return uriInfo;
     }
 }
